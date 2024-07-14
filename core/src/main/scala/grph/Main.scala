27ffe95cb09f410cb6b69e13dd1f746f1d97b65b
Lucas Brancolini <lucas.brancolini@efrei.net>
2024-07-14 19:24:33 +0200

    Fixed Topological + added Dijkstra

--- core/src/main/scala/grph/Main.scala
+++ core/src/main/scala/grph/Main.scala
@@ -1,26 +1,40 @@
 import Grph._
-import zio.json._
-import Grph.Graphviz.toDot
-import operations.BreadthFirstSearch._
-import operations.DepthFirstSearch._
+import operations.Dijkstra._
+import Graphviz._
 import operations.TopologicalSorting._
-import operations.CycleDetection._
-import operations.Floyd._
 
 object Main extends App {
 
-    println("Hello, World!")
-    
+    val graph = DiGraph.empty[Int, UnweightedEdge[Int]]
+    val no1 = 1
+    val no2 = 2
+    val no3 = 3
+    val no4 = 4
+    val no5 = 5
+    val e1 = UnweightedEdge(no1, no2)
+    val e2 = UnweightedEdge(no2, no4)
+    val e3 = UnweightedEdge(no1, no3)
+    val e4 = UnweightedEdge(no4, no5)
+    val e5 = UnweightedEdge(no3, no4)
+    val u = graph.addEdge(e1).addEdge(e2).addEdge(e3).addEdge(e4).addEdge(e5)
+
+    println(u.toDot)
+    println(topologicalSort(u))
     val n1 = 1
     val n2 = 2
     val n3 = 3
 
     val s1 = Set(WeightedEdge(n1, n2, 1), WeightedEdge(n2, n3, 2), WeightedEdge(n3, n1, 3))
-
-    val d1 = DiGraph(Set(n1, n2, n3), s1)
-
-    println(d1.getNeighbors(n1))
+    val d1 = DiGraph(Set(n1, n2, n3), s1) 
 
     val d2 = d1.addEdge(WeightedEdge(n1, n3, 1))
-    println(d2.toDot)
+    println(d1.toDot)
+    
+    // Calcul des distances depuis le nœud source n1
+
+    // Affichage des distances sous forme de chaîne de caractères
+    dijkstra(d1, n1) match {
+      case Left(errMsg) => println(errMsg)
+      case Right(distances) => println(s"Distances from node $n1: [${distances.mkString(", ")}]")
+    }
 }
