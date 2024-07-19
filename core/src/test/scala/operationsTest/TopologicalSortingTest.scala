72587ccebcb4e0cee96fb55212abfdd2f646d6b7
Kurnoth <arnaud.kohler@bbox.fr>
2024-07-19 14:48:46 -0100

    Fix Test topo because class Topo change

--- core/src/test/scala/operationsTest/TopologicalSortingTest.scala
+++ core/src/test/scala/operationsTest/TopologicalSortingTest.scala
@@ -18,8 +18,33 @@ class TopologicalSortingTest extends AnyFlatSpec {
         val e4 = UnweightedEdge(n4, n5)
         val e5 = UnweightedEdge(n3, n4)
         val u = graph.addEdge(e1).addEdge(e2).addEdge(e3).addEdge(e4).addEdge(e5)
-        val result = TopologicalSorting.topologicalSort(u)
+        val result = TopologicalSorting.topologicalSort(u) match {
+            case Left(errMsg) => errMsg
+            case Right(sortedNodes) => sortedNodes
+        }
         val expectedResult = List(n1, n3, n2, n4, n5)
+        assert(result == expectedResult)
+    }
+
+    it should "be able to return error message if there is a cycle" in {
+        val graph = DiGraph.empty[Int, UnweightedEdge[Int]]
+        val n1 = 1
+        val n2 = 2
+        val n3 = 3
+        val n4 = 4
+        val n5 = 5
+        val e1 = UnweightedEdge(n1, n2)
+        val e2 = UnweightedEdge(n2, n4)
+        val e3 = UnweightedEdge(n1, n3)
+        val e4 = UnweightedEdge(n4, n5)
+        val e5 = UnweightedEdge(n3, n4)
+        val e6 = UnweightedEdge(n4, n2)
+        val u = graph.addEdge(e1).addEdge(e2).addEdge(e3).addEdge(e4).addEdge(e5).addEdge(e6)
+        val result = TopologicalSorting.topologicalSort(u) match {
+            case Left(errMsg) => errMsg
+            case Right(sortedNodes) => sortedNodes
+        }
+        val expectedResult = "Error, graph contains a cycle"
         assert(result == expectedResult)
     }
 }
